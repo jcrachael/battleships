1b1f2e373839850618eddf4e071938bc03b361f3
Rachael Cole <jcrachael@gmail.com>
2022-11-12 17:33:28 +1100

    Updates

--- bundle-d65c86794e859019ecad.js
+++ bundle-d65c86794e859019ecad.js
@@ -978,6 +978,9 @@ function Game(name) {
     // Initialise the boards
     let AiBoard = new _Gameboard__WEBPACK_IMPORTED_MODULE_2__["default"](_Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer);
     let humanBoard = new _Gameboard__WEBPACK_IMPORTED_MODULE_2__["default"](humanPlayer);
+    let prevAIturn = {};
+    let prevPrevAIturn = {};
+    let invalidCoords = {};
 
     // Initalise gameOver to be false and winner to be null
     let gameOver = false;
@@ -1028,36 +1031,332 @@ function Game(name) {
         if (attack.resultType === 'sink') {
             let message = 'Jasper says, "You sunk my battleship."'
             display.updateComment(message);
-            setTimeout(function(){
-                console.log('timeout')
-            }, 5000);
-                // Check if shipsLeft on receiving board
-                let remainingShips = AiBoard.shipsLeft();
-                if (remainingShips === 0) {
-                    // If the receiving board has no ships left, the active player wins
-                    winner = humanPlayer;
-                    gameOver = true;
-                    return gameOver;
-                }
-
-            
-            
+            // Check if shipsLeft on receiving board
+            let remainingShips = AiBoard.shipsLeft();
+            if (remainingShips === 0) {
+                // If the receiving board has no ships left, the active player wins
+                winner = humanPlayer;
+                gameOver = true;
+                return gameOver;
+            }
         }
 
         return attack;
     }
+    
+    
+    function checkSmartTurn(usedCoords, coords, adjacents) {
+        // First, check if the supplied coords have been used
+        let isSubArray = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(coords, usedCoords);
+    
+        // base case
+        // If the coords do not match any used coords, they are valid
+        if (isSubArray === false) {
+            // Store the new coords in the usedCoords array because we will use them
+            let newArray = [coords[0], coords[1]]
+            let l = Object.keys(usedCoords).length;
+            usedCoords[l] = newArray;
+            // Return the coords and usedCoords
+            return {coords, usedCoords}
+        } 
+        // If the coords do match, they are invalid
+        // get new smart coords
+        let newCoords = newSmartCoords(coords, adjacents, usedCoords);
+        console.log('New coords: ' + newCoords);
+        // Try to validate again
+        return checkSmartTurn(usedCoords, newCoords, adjacents);
+        
+        
+    }
+
+    // Takes old coords and returns new coords from the same adjacency list
+    function newSmartCoords(oldCoords, adjacents, usedCoords) {
+
+        let l = Object.keys(invalidCoords).length;
+        // store the invalid coords in invalidCoords
+        invalidCoords[l] = oldCoords;
+
+        // get new random coords from the adjacents array
+        let newCoords = smartCoords(adjacents);
+
+        // check if the new coords are in invalidCoords
+        let isNewSub = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(newCoords, invalidCoords);
+
+        // If the new coords are not in the invalidCoords, we can use them
+        if (isNewSub === false) {
+            // We can use these coords
+            return newCoords
+        } 
+
+        // If the length of invalidCoords = the length of adjacents, there are
+        // no valid coords in adjacents, so return random coords
+        if (l == adjacents.length) {
+            // Get random coords
+            newCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomCoords();
+            return newCoords 
+        } 
+        
+        newCoords = smartCoords(adjacents);
+        return newSmartCoords(newCoords, adjacents, usedCoords);
+    }
+
+    // Get random coords from an adjacency list
+    function smartCoords(adjacents) {
+        // Choose a random number between 0 and adjacents.length 
+        let max = adjacents.length;
+        let randomIndex = Math.floor(Math.random() * max);
+        // Set the coords to the randomly chosen index of adjacent squares
+        let coords = adjacents[randomIndex];
+        return coords
+    }
+
+    function getAdjacents(prevCoord) {
+        let row = prevCoord[0];
+        let col = prevCoord[1];
+        let adjacents = [];
+        let topLeft = [0,0];
+        let topRight = [0,9];
+        let bottomLeft = [9,0];
+        let bottomRight = [9,9];
+        let topRow = {
+            0: [0,1],
+            1: [0,2],
+            2: [0,3],
+            3: [0,4],
+            4: [0,5],
+            5: [0,6],
+            6: [0,7],
+            7: [0,8]
+        };
+        let bottomRow = {
+            0: [9,1],
+            1: [9,2],
+            2: [9,3],
+            3: [9,4],
+            4: [9,5],
+            5: [9,6],
+            6: [9,7],
+            7: [9,8]
+        };
+        let leftSide = {
+            0: [1,0],
+            1: [2,0],
+            2: [3,0],
+            3: [4,0],
+            4: [5,0],
+            5: [6,0],
+            6: [7,0],
+            7: [8,0]
+        };
+        let rightSide = {
+            0: [1,9],
+            1: [2,9],
+            2: [3,9],
+            3: [4,9],
+            4: [5,9],
+            5: [6,9],
+            6: [7,9],
+            7: [8,9]
+        };
+        // Get the adjacent squares
+        if (prevCoord == topLeft) {
+            adjacents = [[0,1], [1,0]];
+        } else if (prevCoord == topRight) {
+            adjacents = [[0,8], [1,9]];
+        } else if (prevCoord == bottomLeft) {
+            adjacents = [[8,0], [9,1]];
+        } else if (prevCoord == bottomRight) {
+            adjacents = [[8,9], [9,8]]
+        } else if (_Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(prevCoord, topRow)) {
+            adjacents = [[row, col - 1], [row, col + 1], [row + 1, col]]
+        } else if (_Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(prevCoord, bottomRow)) {
+            adjacents = [[row, col -1], [row, col + 1], [row - 1, col]]
+        } else if (_Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(prevCoord, leftSide)) {
+            adjacents = [[row - 1, col], [row + 1, col], [row, col + 1]]
+        } else if (_Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.isSubset(prevCoord, rightSide)) {
+            adjacents = [[row - 1, col], [row + 1, col], [row, col - 1]]
+        } else {
+            adjacents = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]]
+        }
+
+        console.log('Adjacents: ');
+        console.log(adjacents);
+
+        return adjacents;
+    }
+
 
     // Performs the AI's turn
     function AiTurn() {
 
+        // Update the comment message
         let message = 'Jasper\'s turn...';
         display.updateComment(message);
-        // Get random coords
-        let coords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomCoords();
-        let usedCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.usedCoords;
-        let turn = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomTurn(usedCoords, coords);
-        let aiInput = turn.coords;
-        let aiAttack = humanBoard.receiveAttack(aiInput);
+
+        let turn;
+        let usedCoords;
+        let aiInput;
+        let aiAttack;
+        let adjacents;
+        let coords;
+        
+        
+        let prevObjLength = Object.keys(prevAIturn).length;
+        let prevPrevObjLength = Object.keys(prevPrevAIturn).length;
+
+        
+
+        // If it's the AI's second or later turn
+        if (prevObjLength > 0) {
+
+            console.log('Last AI move was a ' + prevAIturn.attack.resultType);
+            
+            // Get the data from the prevAiTurn
+            let prevResult = prevAIturn.attack.resultType;
+            let prevCoord = prevAIturn.coords;
+            
+            // If it's the AI's third or later turn
+            if (prevPrevObjLength > 0) {
+                console.log('The turn before that was a: ' + prevPrevAIturn.attack.resultType);
+
+                let prevPrevResult = prevPrevAIturn.attack.resultType;
+                let prevPrevCoord = prevPrevAIturn.coords;
+
+                console.log('Prev result: ' + prevResult);
+                console.log('PrevPrev result: ' + prevPrevResult);
+
+                // If the turn before last was a 'hit'
+                if (prevResult == 'miss' && prevPrevResult == 'hit') {
+
+                    console.log('Getting adjacents for prevPrevCoord: ' + prevPrevCoord);
+
+                    // Get the adjacency list for the turn before last
+                    adjacents = getAdjacents(prevPrevCoord);
+
+                    if (adjacents.length > 4) {
+                        let first = adjacents[0];
+                        let second = adjacents[1];
+                        let third = adjacents[2];
+                        let fourth = adjacents[3];
+                        let newAdjList = [first, second, third, fourth];
+                        console.log('New list: ')
+                        console.log(newAdjList);
+                        
+                        adjacents = newAdjList;
+                    }
+                    
+
+                    console.log(prevPrevCoord);
+                    console.log('New adjacents: ' + adjacents);
+                    console.log(adjacents)
+
+                    // Set the coords to a random index in adjacents
+                    usedCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.usedCoords;
+                    coords = smartCoords(adjacents);
+                    // check the turn is valid
+                    turn = checkSmartTurn(usedCoords, coords, adjacents);
+
+                    console.log('This AI turn: ');
+                    console.log(turn);
+
+                    // Save the aiAttack 
+                    let l = Object.keys(usedCoords).length;
+                    usedCoords[l] = turn.coords;
+                    aiAttack = humanBoard.receiveAttack(turn.coords);
+                    aiInput = turn.coords;
+
+                    // Save the old prevAIturn as prevPrevAITurn
+                    prevPrevAIturn['attack'] = prevAIturn['attack'];
+                    prevPrevAIturn['coords'] = prevAIturn['coords'];
+
+                    console.log('PrevPrevAIturn: ');
+                    console.log(prevPrevAIturn);
+
+                    // Save the current attack as the new prevAIturn
+                    prevAIturn['attack'] = aiAttack;
+                    prevAIturn['coords'] = aiInput;
+
+                    console.log('Previous AI turn:');
+                    console.log(prevAIturn);
+                }
+            }
+                
+
+            // If the last turn was a 'hit'
+            if (prevResult == 'hit') {
+                // Get the adjacency list for the last turn
+                adjacents = getAdjacents(prevCoord);
+                
+                // Set the coords to a random index in adjacents
+                usedCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.usedCoords;
+                coords = smartCoords(adjacents);
+                // check the turn is valid
+                turn = checkSmartTurn(usedCoords, coords, adjacents);
+                // Save the aiAttack 
+                let l = Object.keys(usedCoords).length;
+                usedCoords[l] = turn.coords;
+                aiAttack = humanBoard.receiveAttack(turn.coords);
+                aiInput = turn.coords;
+
+                // Save the old prevAIturn as prevPrevAITurn
+                prevPrevAIturn['attack'] = prevAIturn['attack'];
+                prevPrevAIturn['coords'] = prevAIturn['coords'];
+
+                console.log('PrevPrevAIturn: ');
+                console.log(prevPrevAIturn);
+
+                // Save the current attack as the new prevAIturn
+                prevAIturn['attack'] = aiAttack;
+                prevAIturn['coords'] = aiInput;
+
+                console.log('Previous AI turn:');
+                console.log(prevAIturn);
+
+            // If the last AI turn was not a hit, get random coords
+            } else {
+                // Get random coords
+                coords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomCoords();
+                // Get used coords
+                usedCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.usedCoords;
+                // Validate random coords against used coords
+                turn = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomTurn(usedCoords, coords);
+                // Save the validated coords as aiInput
+                aiInput = turn.coords;
+                // Perform the attack 
+                aiAttack = humanBoard.receiveAttack(aiInput);
+
+                // Save the old prevAIturn as prevPrevAITurn
+                prevPrevAIturn['attack'] = prevAIturn['attack'];
+                prevPrevAIturn['coords'] = prevAIturn['coords'];
+
+                console.log('PrevPrevAIturn: ');
+                console.log(prevPrevAIturn);
+
+                // Save the current attack as the new prevAIturn
+                prevAIturn['attack'] = aiAttack;
+                prevAIturn['coords'] = aiInput;
+
+                console.log('Previous AI turn:');
+                console.log(prevAIturn);
+            }
+            
+        // If it is the AI's first turn, get random coords
+        } else {
+            // Get random coords
+            coords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomCoords();
+            // Get the AI's used coords
+            usedCoords = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.usedCoords;
+            // Validate the coords
+            turn = _Player__WEBPACK_IMPORTED_MODULE_0__.AiPlayer.randomTurn(usedCoords, coords);
+            // Set aiInput to validated coords
+            aiInput = turn.coords;
+            // Perform the AI attack on the human board 
+            aiAttack = humanBoard.receiveAttack(aiInput);
+            // Store this attack as the prevAIturn
+            prevAIturn['attack'] = aiAttack;
+            prevAIturn['coords'] = aiInput;
+        }
+        
 
         setTimeout(function() {
 
@@ -1087,9 +1386,13 @@ function Game(name) {
             }
         }, 500)
 
-        
+        let aiTurnRes = {
+            aiAttack: aiAttack,
+            aiInput: aiInput,
+            coords: coords
+        }        
 
-        return {aiAttack, aiInput};
+        return aiTurnRes;
     }
 
     // play a round
@@ -1298,9 +1601,6 @@ function Gameboard(owner) {
                 resultType = 'hit';
                 let ship = board[yCoord][xCoord];
 
-                console.log('A ship was hit:');
-                console.log(ship);
-
                 board[yCoord][xCoord] = 'hit';
                 ship.hit();
                 ship.isSunk();
@@ -1349,7 +1649,7 @@ function Player(name) {
         return coords;
     }
 
-  
+
 
     function randomTurn(usedCoords, coords) {
 
@@ -1376,17 +1676,11 @@ function Player(name) {
         coords = randomCoords();
 
         return randomTurn(usedCoords, coords);
-
-        
-
         
     }
 
     // Returns true if arr1 is a subset of arr2
     function isSubset(coords,usedCoords) {
-
-  
-        console.log('AI clicked: ' + coords);
 
         // Loop through each item in usedCoords
         let l = Object.keys(usedCoords).length;
@@ -1408,6 +1702,7 @@ function Player(name) {
         usedCoords: usedCoords,
         randomCoords: randomCoords,
         randomTurn: randomTurn,
+        isSubset: isSubset,
     }
 }
 
@@ -3097,4 +3392,4 @@ game.init();
 /******/ var __webpack_exports__ = (__webpack_exec__(138));
 /******/ }
 ]);
-//# sourceMappingURL=bundle-21f1b6b112e2259331bf.js.map
+//# sourceMappingURL=bundle-d65c86794e859019ecad.js.map
